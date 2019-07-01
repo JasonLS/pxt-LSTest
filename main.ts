10c8c50b71153a35a577dbc5b36622b6d20ec6a7
JasonLS <jsedluk01@gmail.com>
2019-07-01 10:08:40 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -19,3 +19,34 @@ namespace basic2 {
     //% block
     export function red(){}
 }
+
+/**
+ * Organize your blocks in groups
+ */
+//% color="#AA278D"
+//% groups="['Fruits', 'Veggies']"
+namespace food {
+    //% block
+    //% group="Veggies"
+    export function potato() {
+
+    }
+
+    //% block
+    //% group="Veggies"
+    export function bean() {
+
+    }
+
+    //% block
+    //% group="Fruits"
+    export function apple() {
+
+    }
+
+    //% block
+    //% group="Fruits"
+    export function banana() {
+
+    }
+}
