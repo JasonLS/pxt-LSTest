fc99fdea860ca99c081b25f8fe1341bb363f517a
JasonLS <jsedluk01@gmail.com>
2019-07-01 09:50:46 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
- 
+namespace basic {
+
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
